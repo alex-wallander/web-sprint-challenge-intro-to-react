15828b501ba348cee6a2a7a8731d50b0dd67c51e
alex-wallander <alexwallander@gmail.com>
2021-01-22 12:57:04 -0600

    framework set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import { BASE_URL } from './constants/secret';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,23 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [details, setDetails] = useState([])
+  const getData = () => {
+    axios
+    .get(`${BASE_URL}`)
+    .then((res) => {
+      setDetails(res.data);
+      console.log(res.data)
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+  
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="React Wars >"> </h1>
+
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,10 @@
 // Write your Character component here
+import React from 'react';
+
+export default function Character(props) {
+    return(
+        <div>
+            <h2>{props.name}</h2> <button>{props.birth_year}</button>
+        </div>
+    )
+}
